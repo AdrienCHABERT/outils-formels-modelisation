0433c8eb414759eb6eaae34385ab1f830820a132
AdrienCHABERT <adrien.chabert@etu.unige.ch>
2017-10-26 18:19:45 +0200

    add tp-02

--- tp-02/Smokers/Sources/Smokers/main.swift
+++ tp-02/Smokers/Sources/Smokers/main.swift
@@ -1,8 +1,10 @@
 import PetriKit
 import SmokersLib
-
+import Foundation
 // Instantiate the model.
 let model = createModel()
+
+// Pragrammeur : Adrien Chabert
 
 // Retrieve places model.
 guard let r  = model.places.first(where: { $0.name == "r" }),
@@ -15,14 +17,90 @@ guard let r  = model.places.first(where: { $0.name == "r" }),
       let s2 = model.places.first(where: { $0.name == "s2" }),
       let w3 = model.places.first(where: { $0.name == "w3" }),
       let s3 = model.places.first(where: { $0.name == "s3" })
+
 else {
     fatalError("invalid model")
 }
+  // On enregistre notre réseau de Petri sous forme graphique
+  try model.saveAsDot(to: URL(fileURLWithPath: "model.dot"), withMarking: [r: 1, w1: 1, w2: 1, w3: 1])
 
+let transitions = model.transitions
+
+// Voici ma fonction pour compter le nombre de noeud d'un MarkingGraph. C'est la même fonction qu'on a fait pour la série 5 d'exercice.
+func countNodes(markingGraph: MarkingGraph) -> Int {
+  var seen = [markingGraph]
+  var toVisit = [markingGraph]
+
+  while let current = toVisit.popLast() {
+    for (_, successor) in current.successors{
+      if !seen.contains(where: { $0 === successor}) {
+        seen.append(successor)
+        toVisit.append(successor)
+      }
+    }
+  }
+  return seen.count
+}
 // Create the initial marking.
 let initialMarking: PTMarking = [r: 1, p: 0, t: 0, m: 0, w1: 1, s1: 0, w2: 1, s2: 0, w3: 1, s3: 0]
+print(initialMarking) //
 
-// Create the marking graph (if possible).
+// Voici mon graphe de marquage à l'aide de la fonction dans PTNet+Extensions
 if let markingGraph = model.markingGraph(from: initialMarking) {
-    // Write here the code necessary to answer questions of Exercise 4.
+  // Write here the code necessary to answer questions of Exercise 4.
+  print("Nombre d'états : ",countNodes(markingGraph: markingGraph)) //On affiche le résultat qui est 32 pour notre exercice.
+
+// Question 2 :
+// Voici ma fonction pour savoir si deux personnes peuvent fumer ensenble
+func fumerEnsemble(markingGraph: MarkingGraph) -> Bool {
+  var seen = [markingGraph]
+  var toVisit = [markingGraph]
+
+  while let current = toVisit.popLast() {
+    for (_, successor) in current.successors {
+      if !seen.contains(where: { $0 === successor}) {
+        seen.append(successor)
+        toVisit.append(successor)
+        if ((successor.marking[s1] == 1) && (successor.marking[s2] == 1)) { //Si il y a un état où la personne 1 et 2 fument ensemble.
+          return true
+        } else if ((successor.marking[s1] == 1) && (successor.marking[s3] == 1)) { //Si il y a un état où la personne 1 et 2 fument ensemble.
+          return true
+        } else if ((successor.marking[s2] == 1) && (successor.marking[s3] == 1)) { //Si il y a un état où la personne 1 et 2 fument ensemble.
+         return true
+       }
+      }
+    }
+  }
+  return false
+}
+
+// On voit qu'avec notre model, deux personnes peuvent fumer ensemble.
+print("Est ce que deux personnes peuvent fumer ensemble ? : ", fumerEnsemble(markingGraph: markingGraph))
+
+
+// Question 3 :
+// Voici ma fonction pour savoir si on peut avoir deux ingrédients identiques
+func memeIngredient(markingGraph: MarkingGraph) -> Bool {
+  var seen = [markingGraph]
+  var toVisit = [markingGraph]
+
+  while let current = toVisit.popLast() {
+    for (_, successor) in current.successors {
+      if !seen.contains(where: { $0 === successor}) {
+        seen.append(successor)
+        toVisit.append(successor)
+        if ((successor.marking[m] == 2) || (successor.marking[t] == 2) || (successor.marking[p] == 2)) { //Si il y a 2 fois un même ingrédient
+          return true
+        }
+
+      }
+    }
+  }
+  return false
+}
+
+// On voit qu'avec notre model, il ne peut pas y avoir deux ingérdients identiques.
+print("Est ce qu'il y a deux même ingrédient ? : ", memeIngredient(markingGraph: markingGraph))
+
+
 }
--- tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
+++ tp-02/Smokers/Sources/SmokersLib/PTNet+Extensions.swift
@@ -15,8 +15,30 @@ public class MarkingGraph {
 public extension PTNet {
 
     public func markingGraph(from marking: PTMarking) -> MarkingGraph? {
-        // Write here the implementation of the marking graph generation.
-        return nil
-    }
+        let GraphMarquage = MarkingGraph(marking: marking, successors: [:]) //Il s'agit du graphe de marquage qu'on va retourner.
+        var MarquageATraiter = [GraphMarquage] //Ce sont les marquages qu'on doit traiter
+        var Marquagetraiter = [MarkingGraph]() //Ce sont les marquages qu'on a déjà traiter
 
+        //La boucle pour parcourir tout le graphe
+        repeat {
+        let mm = MarquageATraiter.popLast() //On prend le dernier élément de notre liste à traiter et on l'enlève
+          Marquagetraiter.append(mm!) //On ajoute cette élément à la liste deja traiter
+          let marquage = mm!.marking
+           for trans in self.transitions { //Boucle sur les transitions
+              if let fireable = trans.fire(from: marquage) { //On regarde si une transition est tirable et si oui on garde son marquage pour inscrir son successors
+                if Marquagetraiter.contains(where: {$0.marking == fireable}) { //si ce marquage a déjà été traiter
+                  mm!.successors[trans] = Marquagetraiter.first(where: {$0.marking == fireable})
+                } else if MarquageATraiter.contains(where: {$0.marking == fireable}) { //si ce marquage va être traiter
+                  mm!.successors[trans] = MarquageATraiter.first(where: {$0.marking == fireable})
+                } else {
+                  let n = MarkingGraph(marking: fireable)
+                  mm!.successors[trans] = n
+                  MarquageATraiter.append(n) //On ajoute les nouveaux marquages jamais rencontrer dans la liste à traiter
+                }
+              }
+           }
+         } while (MarquageATraiter.count != 0) //On refait la boucle tant que la liste à traiter n'est pas vide
+
+         return GraphMarquage
+       }
 }
